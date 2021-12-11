e3f7cf54141ae09c9a5a86db2f25018bd7684f90
bomj321 <jmobb612@gmail.com>
2021-12-11 12:54:18 -0400

    [DELETE] Trash code

--- src/layouts/dashboard/SidebarConfigCollaborator.js
+++ src/layouts/dashboard/SidebarConfigCollaborator.js
@@ -1,7 +1,5 @@
 import { Icon } from '@iconify/react';
 import pieChart2Fill from '@iconify/icons-eva/pie-chart-2-fill';
-import peopleFill from '@iconify/icons-eva/people-fill';
-import pieChartFill from '@iconify/icons-eva/pie-chart-fill';
 // import LiveHelpIcon from '@material-ui/icons/LiveHelp';
 import clipboardOutline from '@iconify/icons-eva/clipboard-outline';
 // import alertCircleFill from '@iconify/icons-eva/alert-circle-fill';

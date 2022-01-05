9caeb1bf9e1b3af014d592f67e2593e692bc1db1
bomj321 <jmobb612@gmail.com>
2022-01-05 08:30:16 -0400

    [ADD] Change logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -11,10 +11,9 @@ Logo.propTypes = {
 export default function Logo({ sx }) {
   return (
     <Box
-      className="bg-white"
       component="img"
-      src="https://filesharingdev.nyc3.digitaloceanspaces.com/public/Human2Coach/Human2Coach.ico"
-      sx={{ width: 250, height: 50, ...sx }}
+      src="https://filesharingdev.nyc3.digitaloceanspaces.com/public/Human2Coach/Human2Coach2.ico"
+      sx={{ width: 210, height: 50, ...sx }}
     />
   );
 }
